4c8206d947e261155060d3b00192cbe307f58635
idgm5 <idgm@pm.me>
2020-07-08 11:37:32 -0500

    Adding more fixes

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
 var express = require('express');
-var server = express()
+var http = require('http');
+var app = express();
+
+app.use(express.static('public'));
+app.get('/', function(request, response){
+    response.send('Game running');
+});
+
+var server = http.Server(app);
 
 server.listen(process.env.PORT || 3000, function(){
    var host = server.address().address;
