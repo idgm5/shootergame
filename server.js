510cc8e20702f2af5cae941d93f357de567d8759
idgm5 <idgm@pm.me>
2020-07-08 11:24:20 -0500

    Add server files

---
+++ server.js
@@ -0,0 +1,5 @@
+server.listen(process.env.PORT || 3000, function(){
+   var host = server.address().address;
+   var port = server.address().port;
+   console.log('Listening on http://%s:%s', host, port);
+});
