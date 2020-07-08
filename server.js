4faeb1b0528e2acf9869ce739f2e969b1fd95994
idgm5 <idgm@pm.me>
2020-07-08 11:28:13 -0500

    Fix server files

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+var express = require('express');
+var server = express()
+
 server.listen(process.env.PORT || 3000, function(){
    var host = server.address().address;
    var port = server.address().port;
