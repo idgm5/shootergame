d98870ab3b6996df40a4683c96444ea436b0ccb1
idgm5 <idgm@pm.me>
2020-07-07 22:09:10 -0500

    Fix bug with alternative ending

--- src/Scenes/SceneScores.js
+++ src/Scenes/SceneScores.js
@@ -18,8 +18,7 @@ async function sendScore(name, score) {
 }
 
 var zero = 0;
-const currentScore = JSON.parse(localStorage.getItem('currentScore'));
-const leftAmmo = JSON.parse(localStorage.getItem('Ammunition'));
+const currentScore = JSON.parse(localStorage.getItem('currentScore'));;
 
 export default class SceneScores extends Phaser.Scene {
   constructor() {
@@ -33,27 +32,6 @@ export default class SceneScores extends Phaser.Scene {
     this.input.keyboard.removeCapture(Phaser.Input.Keyboard.KeyCodes.S);
     this.input.keyboard.removeCapture(Phaser.Input.Keyboard.KeyCodes.A);
     this.input.keyboard.removeCapture(Phaser.Input.Keyboard.KeyCodes.D);
-
-    if(leftAmmo <= zero){
-      this.title = this.add.text(this.game.config.width * 0.5, 128, "NO AMMO LEFT", {
-        fontFamily: 'monospace',
-        fontSize: 48,
-        fontStyle: 'bold',
-        color: '#ffffff',
-        align: 'center'
-      });
-      this.title.setOrigin(0.5);
-    } else {
-      this.title = this.add.text(this.game.config.width * 0.5, 128, "GAME OVER", {
-        fontFamily: 'monospace',
-        fontSize: 48,
-        fontStyle: 'bold',
-        color: '#ffffff',
-        align: 'center'
-      });
-      this.title.setOrigin(0.5);
-    }
-
 
     const div = document.createElement('div');
     div.innerHTML = `<input type='search' placeholder='Write your name' id='tag'
@@ -100,5 +78,28 @@ export default class SceneScores extends Phaser.Scene {
     });
     this.high.setText('HIGHEST: ' + lasthigh);
 
+  }
+
+  update(){
+    const leftAmmo = JSON.parse(localStorage.getItem('Ammunition'))
+    if(leftAmmo <= zero){
+      this.title = this.add.text(this.game.config.width * 0.5, 128, "NO AMMO LEFT", {
+        fontFamily: 'monospace',
+        fontSize: 48,
+        fontStyle: 'bold',
+        color: '#ffffff',
+        align: 'center'
+      });
+      this.title.setOrigin(0.5);
+    } else {
+      this.title = this.add.text(this.game.config.width * 0.5, 128, "GAME OVER", {
+        fontFamily: 'monospace',
+        fontSize: 48,
+        fontStyle: 'bold',
+        color: '#ffffff',
+        align: 'center'
+      });
+      this.title.setOrigin(0.5);
+    }
   }
 }
